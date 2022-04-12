c35b346fda58cfb0a11b6733cdea9420f3495101
aaronfraint <aaronfraint@icloud.com>
2022-04-12 14:43:55 -0400

    return stop data as singlepart points

--- philly_transit_data/scrape_data.py
+++ philly_transit_data/scrape_data.py
@@ -6,62 +6,57 @@ import geopandas as gpd
 
 
 class TransitData:
-    """ Provide a single method for downloading one or all
-        transit datasets in the Philly region
+    """Provide a single method for downloading one or all
+    transit datasets in the Philly region
     """
 
     URL_CODES = {
         "SEPTA regional rail": {
             # Latest data published 12/17/21 (as of 2/3/2022)
             "stops": "cf49c62fbc17430aa818e900556d207e_0",
-            "lines": "7ebff6bc356d4fa28d4a7e4147d03b32_0" 
+            "lines": "7ebff6bc356d4fa28d4a7e4147d03b32_0",
         },
         "SEPTA trolley": {
             # Latest data published 12/17/21 (as of 2/3/2022)
-            "stops": "20e09781b71c4800a413796662646129_0", 
-            "lines": "33944ef79d2249aca38561a68dc3e06f_0"
+            "stops": "20e09781b71c4800a413796662646129_0",
+            "lines": "33944ef79d2249aca38561a68dc3e06f_0",
         },
         "SEPTA bus": {
             # Latest data published 12/17/21 (as of 2/3/2022)
             "stops": "05374634f1ed45f7a910fcf2599545b9_0",
-            "lines": "06ad85efda2a4fc6bdaee0c146339624_0"
+            "lines": "06ad85efda2a4fc6bdaee0c146339624_0",
         },
         "SEPTA highspeed": {
             # Latest data published 2/2/22 (as of 2/3/2022)
             "stops": "af52d74b872045d0abb4a6bbbb249453_0",
-            "lines": "1e7754ca5f7d47e480a628e282466428_0" 
-
+            "lines": "1e7754ca5f7d47e480a628e282466428_0",
         },
         "NJT bus": {
             # Latest data published 9/29/21 (as of 2/3/2022)
-            "stops": "cb4184c3bb34488097d15138ce53a52e_0", 
-            "lines": "96fc4941f51e46dab452ac8509edb0cc_0"
+            "stops": "cb4184c3bb34488097d15138ce53a52e_0",
+            "lines": "96fc4941f51e46dab452ac8509edb0cc_0",
         },
         "NJT rail": {
             # Latest data published 9/29/21 (as of 2/3/2022)
             "stops": "4809dada94c542e0beff00600ee930f6_0",
-            "lines": "e6701817be974795aecc7f7a8cc42f79_0"
+            "lines": "e6701817be974795aecc7f7a8cc42f79_0",
         },
         "NJT light rail": {
             # Latest data published 8/25/21 (as of 2/3/2022)
             "stops": "7877bb73757d4b1586338ccf2168705d_0",
-            "lines": "b432bf3bf40f40da9d43fa955b834274_0"
+            "lines": "b432bf3bf40f40da9d43fa955b834274_0",
         },
     }
 
     def _make_url(self, code: str):
-        """ Make the URL by placing the code into the ArcGIS opendata pattern """
+        """Make the URL by placing the code into the ArcGIS opendata pattern"""
         return f"https://opendata.arcgis.com/datasets/{code}.geojson"
 
     def options(self):
-        """ Return a list of all modes that can be scraped """
+        """Return a list of all modes that can be scraped"""
         return list(self.URL_CODES.keys())
 
-    def add_data_source(
-            self,
-            mode_name: str,
-            url_type: str,
-            url_or_code: str):
+    def add_data_source(self, mode_name: str, url_type: str, url_or_code: str):
         """Allow users to add their own URLS to ArcGIS open data.
 
         :param mode_name: name of the provider and mode, eg. 'SEPTA bus'
@@ -80,8 +75,7 @@ class TransitData:
             return None
 
         # Let the user know if they're going to overwrite an existing code
-        if mode_name in self.URL_CODES \
-           and url_type in self.URL_CODES[mode_name]:
+        if mode_name in self.URL_CODES and url_type in self.URL_CODES[mode_name]:
             print(f"{mode_name} - {url_type} already exists: Overwriting URL")
 
         # Add the mode if it's not already in the dictionary
@@ -98,14 +92,16 @@ class TransitData:
         self.URL_CODES[mode_name][url_type] = code
 
     def get_data_from_portal(self, mode: str = "SEPTA bus"):
-        """ Extract stop and line data from .geojson URLs
-            Return two geodataframes: stops and lines
+        """Extract stop and line data from .geojson URLs
+        Return two geodataframes: stops and lines
         """
 
         try:
             url_codes = self.URL_CODES[mode]
         except KeyError:
-            print(f"Mode '{mode}' is not defined. Check your spelling or add a new entry with add_data_source() ")
+            print(
+                f"Mode '{mode}' is not defined. Check your spelling or add a new entry with add_data_source() "
+            )
             return None
 
         stop_url = self._make_url(url_codes["stops"])
@@ -122,9 +118,9 @@ class TransitData:
         return stops, lines
 
     def all_spatial_data(self):
-        """ Download data for all modes.
-            Return single stop and line geodataframes
-            with all SEPTA modes merged together.
+        """Download data for all modes.
+        Return single stop and line geodataframes
+        with all SEPTA modes merged together.
         """
 
         stops = []
@@ -136,4 +132,10 @@ class TransitData:
             stops.append(gdf_stop)
             lines.append(gdf_line)
 
-        return pd.concat(stops), pd.concat(lines)
+        # Convert stop data into a singlepart geodataframe
+        stop_gdf = pd.concat(stops)
+        singleparts = stop_gdf[stop_gdf.geom_type != "MultiPoint"]
+        multiparts = stop_gdf[stop_gdf.geom_type == "MultiPoint"]
+        stops_singlepart = pd.concat([singleparts, multiparts.explode(index_parts=True)])
+
+        return stops_singlepart, pd.concat(lines)
