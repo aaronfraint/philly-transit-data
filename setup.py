f505746354fc21760f53660af672b861031622e2
aaronfraint <aaronfraint@icloud.com>
2022-04-12 14:44:02 -0400

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='philly_transit_data',
     packages=find_packages(),
-    version='0.0.2',
+    version='0.0.3',
     description='Download SEPTA, NJ Transit, and PATCO data from the web',
     author='Aaron Fraint, AICP',
     license='MIT',
